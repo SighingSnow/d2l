a46e3e61b2e1e6e01defedc1e73fc30271fea9ca
SighingSnow <1263750383@qq.com>
2023-07-27 10:40:01 +0800

    zh8.3

--- dl/torch.py
+++ dl/torch.py
@@ -3,10 +3,33 @@ import torch
 import torchvision
 import numpy as np
 import inspect
+import collections
+import re
 from torchvision import transforms
 from d2l import torch as d2l
 from IPython import display
 from dl import torch as dl
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0],list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+def tokenize(lines,token='word'):
+    if token=='word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print('error')
+
+def read_time_machine():
+    d2l.DATA_HUB['time_machine'] = (d2l.DATA_URL + 'timemachine.txt',
+                                '090b5e7e70c295757f55df93cb0a180b9691891a')
+    with open(d2l.download('time_machine'), 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
 # Save hyper paramters
 class HyperParameters:
     def save_hyperparameters(self,ignore=[]):
@@ -89,6 +112,43 @@ class LinearRegressScratch(d2l.Module):
     
     def configure_optimizers(self):
         return torch.optim.SGD([self.w,self.b],lr=self.lr)
+
+class Vocab:
+    def __init__(self,tokens=None,min_freq=0,reserverd_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserverd_tokens is None:
+            reserverd_tokens = []
+        counter = count_corpus(tokens)
+        self._token_freq = sorted(counter.items(),
+                                  key = lambda x:x[1],reverse=True)
+        self.idx_to_token = ['<unk>'] + reserverd_tokens
+        self.token_to_idx = {token: idx 
+                             for idx,token in enumerate(self.idx_to_token)}
+        for token,freq in self._token_freq:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.token_to_idx[token] = len(self.idx_to_token)
+                self.idx_to_token.append(token)
+    
+    def __getitem__(self,tokens):
+        if not isinstance(tokens,(list,tuple)):
+            return self.token_to_idx.get(tokens,self.unk)
+        return [self.__getitem__(token) for token in tokens]
+    
+    def to_tokens(self,indices):
+        if not isinstance(indices,(tuple,list)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[idx] for idx in indices]
+    
+    @property
+    def unk(self):
+        return 0
+    
+    @property
+    def token_freqs(self):
+        return self._token_freq
 
 # loss
 def MSELoss(y_hat,y):
---
+++ zh/ch8/8-3.py
@@ -0,0 +1,75 @@
+import random
+import torch
+from d2l import torch as d2l
+
+import sys
+sys.path.append("../..")
+from dl import torch as dl
+
+tokens = dl.tokenize(dl.read_time_machine())
+corpus = [token for line in tokens for token in line]
+vocab = dl.Vocab(corpus)
+print(vocab.token_freqs[:10])
+
+freqs = [freq for token,freq in vocab.token_freqs]
+# d2l.plot(freqs,xlabel = 'token: x', ylabel='frequency: n(x)',
+#          xscale='log',yscale='log')
+# d2l.plt.show()
+
+# 从copus生成一个大小为batch_size的样本
+# @param batch_size: 每个子序列 样本的数量
+# @param num_steps : 每个子序列中预定义的时间步数
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    # 取随机偏移量
+    corpus = corpus[random.randint(0,num_steps-1):]
+    # 子序列数量
+    num_subseqs = (len(corpus)-1) // num_steps
+    # len(inital_indices) = num_steps
+    initial_indices = list(range(0,num_steps*num_subseqs,num_steps))
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        return corpus[pos:pos+num_steps]
+    
+    # 每次挑选batch_size个子序列
+    num_batches = num_subseqs // batch_size
+    for i in range(0,batch_size*num_batches,batch_size):
+        initial_indices_per_batch = initial_indices[i:i+batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j+1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+# 顺序分区：保证连续性
+def seq_data_iter_sequential(corpus,batch_size,num_steps):
+    # 这里的offset指的是从第几个token开始
+    offset = random.randint(0,num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = torch.tensor(corpus[offset:offset+num_tokens])
+    Ys = torch.tensor(corpus[offset+1:offset+num_tokens+1])
+    Xs,Ys = Xs.reshape(batch_size,-1), Ys.reshape(batch_size,-1)
+    # Xs.shape[1] = num_tokens / batch_size
+    num_batchs = Xs.shape[1] // num_steps
+    for i in range(0,num_batchs * num_steps,num_steps):
+        X = Xs[:,i:i+num_steps]
+        Y = Ys[:,i:i+num_steps]
+        yield X,Y
+
+class SeqDataLoader:
+    def __init__(self,batch_size,num_steps,use_random_iter,max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        # self.corpus, self.vocab = d2l.l
+        self.batch_size, self.num_steps = batch_size,num_steps
+    
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus,self.batch_size,self.num_steps)
+
+
+myseq = list(range(35))
+for X,Y in seq_data_iter_random(myseq,batch_size=2,num_steps=5):
+    print('X: ',X,'\nY: ',Y)
+
+for X,Y in seq_data_iter_sequential(myseq,batch_size=2,num_steps=5):
+    print('X:',X,'\nY:',Y)
